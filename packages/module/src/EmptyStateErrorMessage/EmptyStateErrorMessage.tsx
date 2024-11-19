ce95fac6dd3f9e8dc35d755de9f1a643123e96cc
nicolethoen <nthoen@redhat.com>
2024-11-18 20:57:55 -0500

    fix build errors

--- packages/module/src/EmptyStateErrorMessage/EmptyStateErrorMessage.tsx
+++ packages/module/src/EmptyStateErrorMessage/EmptyStateErrorMessage.tsx
@@ -3,14 +3,14 @@ import ErrorState from "@patternfly/react-component-groups/dist/dynamic/ErrorSta
 import { EmptyStateProps } from '@patternfly/react-core';
 import { PathMissingIcon } from '@patternfly/react-icons';
 
-export interface EmptyStateErrorMessageProps extends Omit<EmptyStateProps, 'children' | 'titleText'> {
+export type EmptyStateErrorMessageProps = {
   /** Optional content to be rendered at the bottom of the error message. */
   children?: React.ReactNode;
   /** Content to be rendered as the Title of the error message, below the image. */
   title?: string;
   /** Content to be rendered as the description of the error below the title. */
   bodyText?: string;
-};
+} & Omit<EmptyStateProps, 'children' | 'titleText'>;
 
 export const EmptyStateErrorMessage: React.FunctionComponent<EmptyStateErrorMessageProps> = ({
   title,
