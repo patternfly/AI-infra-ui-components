0a5dccc1f85c7e1f78558907c555df692163eede
nicolethoen <nthoen@redhat.com>
2024-11-18 20:45:23 -0500

    use react-component-groups

--- packages/module/patternfly-docs/content/examples/EmptyStateErrorMessage/EmptyStateErrorMessageBasic.tsx
+++ packages/module/patternfly-docs/content/examples/EmptyStateErrorMessage/EmptyStateErrorMessageBasic.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { EmptyStateErrorMessage } from "@patternfly/ai-infra-ui-components";
 
 export const EmptyStateErrorMessageBasic: React.FunctionComponent = () => (
-        <EmptyStateErrorMessage
-            title="Error loading workloads" 
-            bodyText="Check your network connection." 
-        >
-            Reach out for more assistance.
-        </EmptyStateErrorMessage>
-    );
+    <EmptyStateErrorMessage
+        title="Error loading workloads" 
+        bodyText="Check your network connection." 
+    >
+        Reach out for more assistance.
+    </EmptyStateErrorMessage>
+);
--- packages/module/src/EmptyStateErrorMessage/EmptyStateErrorMessage.tsx
+++ packages/module/src/EmptyStateErrorMessage/EmptyStateErrorMessage.tsx
@@ -1,35 +1,30 @@
 import * as React from 'react';
-import {
-  EmptyState,
-  EmptyStateBody,
-  Stack,
-  StackItem,
-  EmptyStateFooter,
-} from '@patternfly/react-core';
+import ErrorState from "@patternfly/react-component-groups/dist/dynamic/ErrorState";
+import { EmptyStateProps } from '@patternfly/react-core';
 import { PathMissingIcon } from '@patternfly/react-icons';
 
-export type EmptyStateErrorMessageProps = {
+export interface EmptyStateErrorMessageProps extends Omit<EmptyStateProps, 'children' | 'titleText'> {
   /** Optional content to be rendered at the bottom of the error message. */
   children?: React.ReactNode;
   /** Content to be rendered as the Title of the error message, below the image. */
-  title: string;
-  /** Content to be rendered as the description of the error below the title.*/
-  bodyText: string;
+  title?: string;
+  /** Content to be rendered as the description of the error below the title. */
+  bodyText?: string;
 };
 
 export const EmptyStateErrorMessage: React.FunctionComponent<EmptyStateErrorMessageProps> = ({
   title,
   bodyText,
   children,
+  ...props
 }: EmptyStateErrorMessageProps) => (
-  <EmptyState headingLevel="h2" icon={PathMissingIcon} titleText={title}>
-    <EmptyStateFooter>
-      <Stack hasGutter>
-        <StackItem>
-          <EmptyStateBody>{bodyText}</EmptyStateBody>
-        </StackItem>
-        {children && <StackItem>{children}</StackItem>}
-      </Stack>
-    </EmptyStateFooter>
-  </EmptyState>
+  <ErrorState 
+    titleText={title}
+    bodyText={bodyText} 
+    headingLevel='h2'
+    icon={PathMissingIcon}
+    status="none"
+    customFooter={children || <div></div>}
+    {...props}
+  />
 );
